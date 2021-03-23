f569aaffb80f7636f00c79b6c925cbd0d2ddad3e
Lucas Pires <luc_50@outlook.com>
2021-03-23 19:05:22 -0300

    função input

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,3 +4,7 @@ print("*********************************")
 
 nomero_secreto = 42
 
+# Função para o usuário para usuário inserir a informação solicita
+chute_usuario = input("Digiteo seu número: ")
+
+print("Você digitou: ", chute_usuario)
