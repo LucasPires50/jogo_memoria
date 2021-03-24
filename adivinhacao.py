1599744fd690b80d11ec104bcc2493f25f1437a6
Lucas Pires <luc_50@outlook.com>
2021-03-24 20:55:48 -0300

    função abx e implemantando

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,6 +7,7 @@ print("*********************************")
 # função rondom para gerar número aleatórios
 nomero_secreto = round(random.randrange(1, 101))
 total_de_tentativas = 3
+pontos = 1000
 
 print("Escolha um nivel de dificuldade: ")
 print("(1)Fácil (2)Médio (3)Difícil")
@@ -42,13 +43,16 @@ for rodada in range(1, total_de_tentativas + 1):
     chute_menor = numero_chute < nomero_secreto
 
     if (acertou):
-        print("Você acertou!!")
+        print("Você acertou e fez {} pontos!!".format(pontos))
         break
     else:
         if(chute_maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif(chute_menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
+        # Função abs() sempre deixa o número positivo
+        pontos_perdidos = abs(numero_chute - nomero_secreto)
+        pontos = pontos - pontos_perdidos
 
 print("*********************************")
 print("Fim do jogo!")
